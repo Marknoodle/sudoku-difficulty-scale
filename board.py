01884376d64661c8f8831f1e5c003f738a55c07e
Mark Holcomb <mholcomb@augusta.edu>
2022-04-11 15:57:42 -0400

    Finished the check command, can now be used to check board validity

--- board.py
+++ board.py
@@ -1,5 +1,6 @@
 import random
-from numpy import numpy.transpose
+import numpy
+import math
 class Board:
     # style         -> the kind of game to be played, default is "standard" which is 9x9
     # block_size    -> the size of each subgrid, used to generate the board
@@ -8,27 +9,28 @@ class Board:
     # subgrids      -> a list of pairs, where the first element is the initial index
     #                  of a subgrid, and the second is the final. i.e.
     #                  ((1,3),(1,3)) is the upper left subgrid, ((7,9),(7,9)) is the bottom right 
-    __init__(style='standard', board=None):
-        self.style = style
-        if style == 'standard':
-            self.block_size = 3
+    def __init__(self, board=None, block_size=3):
+        self.block_size = block_size
         if board == None:
             self.board = [[0] * 3 * self.block_size ] * 3 * self.block_size
         else:
             self.board = board
-        self.subgrids = []
-        for x in range(1, (1 + self.block_size) * 3,3):
-            for y in range(1, (1 + self.block_size) * 3,3):
-                self.subgrids = self.subgrids + [((x, x + self.block_size),(y, y + self.block_size))]
-
-    def board_check():
+        self.subgrid_starts = set([math.ceil(x/self.block_size) for x in range (len(self.board))])
+        self.subgrid_rowcoords = [list(range(x,x+3)) for x in self.subgrid_starts]
+        
+    numpy.warnings.filterwarnings('ignore', category=numpy.VisibleDeprecationWarning)
+    def check(self):
         for row in self.board:
             if len(row) > len(set(row)):
                 return False
         for row in numpy.transpose(self.board):
             if len(row) > len(set(row)):
                 return False
-        subgrids = [[self.board[x:x+3][y:y+3] for x in range(1,(self.block_size * 3) + 1,3) for y in range(1,(self.block_size * 3) + 1,3)]]
+        
+        subgrids = []
+        for rows in self.subgrid_rowcoords:
+            for cols in self.subgrid_rowcoords:
+                subgrids.append([self.board[row][col] for row in rows for col in cols])
         for row in subgrids:
             if len(row) > len(set(row)):
                 return False
