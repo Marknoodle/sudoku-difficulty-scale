679cce5b8a7f333b320c42d038db2b19db3dc004
Mark Holcomb <mholcomb@augusta.edu>
2022-04-20 13:01:38 -0400

    filed out template for skills.py

---
+++ skills.py
@@ -0,0 +1,63 @@
+#   Structure of a skill:
+#   Part 1: Find where the skill can be applied (problem recognition)
+#   Part 2: Apply skill
+
+
+def hidden_single():
+    '''
+    description:    While there are technically multiple valid options
+                    there is only one viable solution due to factors
+                    that eliminate values in its subgrid/row/column
+    input: 
+    operations:
+
+    output: True if success, False if failed
+    '''
+    None
+
+def naked_single():
+    '''
+    description:
+    input:
+    operations:
+    output: True if success, False if failed
+    '''
+    None
+
+def naked_pair():
+    '''
+    description:
+    input:
+    operations:
+    output: True if success, False if failed
+    '''
+    None
+
+def hidden_pair():
+    '''
+    description:
+    input:
+    operations:
+    output: True if success, False if failed
+    '''
+    None
+
+def x_wing():
+    '''
+    description: (explain the theory)
+    input: four corners
+    operations:
+        Count steps
+        apply the operation (extend this)
+    output: True if success, False if failed
+    '''
+    None
+
+def swordfish():
+    '''
+    description:
+    input:
+    operations:
+    output: True if success, False if failed
+    '''
+    None
