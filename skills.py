48d3c294f31e3781e53fcf171420db705e84a2b5
Mark Holcomb <mholcomb@augusta.edu>
2022-04-27 13:03:59 -0400

    added the base methods for skill functions

--- skills.py
+++ skills.py
@@ -1,6 +1,54 @@
 #   Structure of a skill:
 #   Part 1: Find where the skill can be applied (problem recognition)
 #   Part 2: Apply skill
+import math
+
+def available_nums(board, cell):
+    '''
+    Returns the intersection of the valid values given by the row, column, and subgrid
+    '''
+    return list(set(search_row(board,cell)) & set(search_col(board, cell)) & set(search_subgrid(board, cell)))
+
+def search_row(board, cell):
+    '''
+    Returns the viable values given other known values in the row
+    '''
+    viable_vals = [1,2,3,4,5,6,7,8,9]
+    row_num = cell[0]
+    for block in board[row_num]:
+        if block in viable_vals:
+            viable_vals.remove(block)
+    return viable_vals
+
+def search_col(board, cell):
+    '''
+    Returns the viable values given other known values in the column
+    '''
+    flipped_board = math.transpose(board)
+    return search_row(flipped_board, cell)
+
+def search_subgrid(board, cell):
+    '''
+    Returns the viable values given other known values in the subgrid
+    '''
+    viable_vals = [1,2,3,4,5,6,7,8,9]
+    row = cell[0]
+    col = cell[1]
+    for block in board.subgrids[3 * (math.floor(row/3)) + (math.floor(col/3))]:
+        if block in viable_vals:
+            viable_vals.remove(block)
+    return viable_vals
+
+def possible_vals(board):
+    '''
+    Returns a bord where each cell has a list of the possible values in said spot
+    '''
+    possible_board = [[],[],[],[],[],[],[],[],[]]
+    for row,row_num in enumerate(board):
+        for col,col_num in enumerate(board):
+            possible_board[row_num][col_num] = available_nums(row_num,col_num)
+    return possible_board
+
 
 
 def hidden_single():
