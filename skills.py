d755f13e3187c6c1a3fefc2994b1bfb9649bebfa
Mark Holcomb <mholcomb@augusta.edu>
2022-05-01 14:18:57 -0400

    added functions which identify if a hidden single, pair, or naked single, or pair is present for a cells row, column, or subgrid

--- skills.py
+++ skills.py
@@ -32,9 +32,7 @@ def search_subgrid(board, cell):
     Returns the viable values given other known values in the subgrid
     '''
     viable_vals = [1,2,3,4,5,6,7,8,9]
-    row = cell[0]
-    col = cell[1]
-    for block in board.subgrids[3 * (math.floor(row/3)) + (math.floor(col/3))]:
+    for block in ret_subgrid(board,cell):
         if block in viable_vals:
             viable_vals.remove(block)
     return viable_vals
@@ -49,7 +47,66 @@ def possible_vals(board):
             possible_board[row_num][col_num] = available_nums(row_num,col_num)
     return possible_board
 
+def ret_subgrid(board, cell):
+    row = cell[0]
+    col = cell[1]
+    return board.subgrids[3 * (math.floor(row/3)) + (math.floor(col/3))]
 
+def ret_column(board, cell):
+    temp = math.transpose(board)
+    return temp[cell[1]]
+
+def subgroups_of_two(target):
+    return [target[i: j] for i in range(len(target))
+                         for j in range(i + 1, len(target) + 1) if len(target[i: j]) == 2]
+
+def find_naked_pair(board, cell):
+    row = cell[0]
+    col = cell[1]
+    target = board[row][col]
+    
+    for block, block_num in enumerate(board[row]):
+        if block == target and block_num != col:
+            return True
+    for block, block_num in enumerate(ret_column(board, cell)):
+        if block == target and block_num != row:
+            return True
+    for block, block_num in enumerate(ret_subgrid(board, cell)):
+        if block == target and block_num != (3 * (math.floor(row/3)) + (math.floor(col/3))):
+            return True
+    return False
+
+def find_hidden_pair(board, cell):
+    row = cell[0]
+    col = cell[1]
+    target = board[row][col]
+    if len(target) >= 2:
+        possible_pairs = subgroups_of_two(target)
+        for block, block_num in enumerate(board[row]):
+            if len(block) > 1:
+                block_pairs = subgroups_of_two(block)
+                for block_pair in block_pairs:
+                    for possible_pair in possible_pairs:
+                        if block_pair == possible_pair and block_num != col:
+                            return True
+        for block, block_num in enumerate(ret_column(board,cell))
+            if len(block) > 1:
+                block_pairs = subgroups_of_two(block)
+                for block_pair in block_pairs:
+                    for possible_pair in possible_pairs:
+                        if block_pair == possible_pair and block_num != row:
+                            return True
+        for block, block_num in enumerate(ret_subgrid(board, cell))
+            if len(block) > 1:
+                block_pairs = subgroups_of_two(block)
+                for block_pair in block_pairs:
+                    for possible_pair in possible_pairs:
+                        if block_pair == possible_pair and block_num != block_num != (3 * (math.floor(row/3)) + (math.floor(col/3))):
+                            return True
+    return False
+    
+                    
+            
 
 def hidden_single(board, cell):
     '''
